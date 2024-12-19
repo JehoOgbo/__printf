17342e4e2dd4442f9236a118d6b897cca50abcf4
Urom Jehoshaphat <uromjehoshaphat@gmail.com>
2024-12-19 15:19:03 +0100

    Add upper limit on size of buffer stdout is being read into

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <unistd.h>
+#include <stdlib.h>
 
 /**
  * _putchar - outputs a single character
@@ -14,6 +15,12 @@ int _putchar(char c, char **buffer, unsigned int *old_size)
 {
 	char *ptr;
 
+	if (*old_size == 1024)
+	{
+		*old_size = 0;
+		write(STDOUT_FILENO, buffer, 1023);
+		free(*buffer);
+	}
 	ptr = _realloc(*buffer, *old_size, *old_size + 1);
 	ptr[*old_size] = c;
 
